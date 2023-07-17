b9bf9c5404212095d80b21a35e9c1b5005334b12
genarossi19 <genaros.gr@gmail.com>
2023-07-17 01:15:25 -0300

    primer scanActivity

--- app/src/main/java/com/genarossi19/suboscan/MainActivity.kt
+++ app/src/main/java/com/genarossi19/suboscan/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.genarossi19.suboscan
+
+
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
-import com.genarossi19.suboscan.ResultActivity
+
 
 import com.genarossi19.suboscan.databinding.ActivityMainBinding
-import com.google.zxing.integration.android.IntentIntegrator
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,39 +21,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnScanFrontal.setOnClickListener { initScannerFrontal() }
-        binding.btnScan.setOnClickListener { initScanner() }
+        binding.btnScanFrontal.setOnClickListener {}
+        binding.btnScan.setOnClickListener { navigateToScan() }
     }
 
-    private fun initScannerFrontal() {
-        val integrator = IntentIntegrator(this)
-        integrator.setPrompt("ESCANEE QR EN EL RECUADRO")
-        integrator.setCameraId(1) // Especifica el ID de la cámara frontal
-        integrator.initiateScan()
+
+    private fun navigateToScan(){
+        val intent = Intent(this, ScanActivity::class.java)
+        startActivity(intent)
     }
 
-    private fun initScanner() {
-        val integrator = IntentIntegrator(this)
-        integrator.setPrompt("ESCANEE QR EN EL RECUADRO")
-        integrator.initiateScan()
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
-            } else {
-                val scanResult = result.contents
-                if (scanResult == "FINISH") {
-                    Toast.makeText(this, "Escaneo finalizado", Toast.LENGTH_SHORT).show()
-                    // Agrega aquí la lógica que deseas ejecutar al escanear el código "FINISH"
-                } else {
-                    Toast.makeText(this, "El valor escaneado es: $scanResult", Toast.LENGTH_LONG).show()
-                    // Aquí puedes agregar la lógica correspondiente al resultado del escaneo
-                }
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/genarossi19/suboscan/ScanActivity.kt
@@ -0,0 +1,95 @@
+package com.genarossi19.suboscan
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+
+
+class ScanActivity : AppCompatActivity() {
+    private lateinit var codeScanner: CodeScanner
+    private val CAMERA_PERMISSION_REQUEST_CODE = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            setupCamera()
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupCamera()
+            } else {
+                Toast.makeText(this, "Permiso de la cámara denegado", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun setupCamera() {
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parámetros (valores predeterminados)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(this, "Error de inicialización de la cámara: ${it.message}",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+}
